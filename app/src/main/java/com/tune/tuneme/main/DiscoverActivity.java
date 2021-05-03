3793e16bcc53cec42cab529fdd8d176e8ca5fb13
Noah <43308031+noahweasley@users.noreply.github.com>
2021-05-02 18:10:41 -0700

    Added Discover activity

---
+++ app/src/main/java/com/tune/tuneme/main/DiscoverActivity.java
@@ -0,0 +1,30 @@
+package com.tune.tuneme.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tune.tuneme.R;
+import com.tune.tuneme.databinding.DiscoverBinding;
+
+@SuppressWarnings("ConstantConditions")
+public class DiscoverActivity extends AppCompatActivity {
+    private DiscoverBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DiscoverBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setTitle(R.string.discover);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
