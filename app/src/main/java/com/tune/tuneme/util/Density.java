65d98d3cf829e5c8568658582d76be94a4fbb37c
Noah <43308031+noahweasley@users.noreply.github.com>
2021-05-07 05:14:33 -0700

    Delete Density.java

--- app/src/main/java/com/tune/tuneme/util/Density.java
+++
@@ -1,8 +0,0 @@
-package com.tune.tuneme.util;
-
-public class Density {
-    public static final int LDPI_LOWER = 320;
-    public static final int LDPI_HIGHER = 400;
-    public static final int XXHDPI = 560;
-    public static final int MDPI = 480;
-}
