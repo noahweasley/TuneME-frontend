299faf7248000832e375259a661bcd6b7b30cfaa
Noah <43308031+noahweasley@users.noreply.github.com>
2021-05-07 04:13:14 -0700

    Provided utility class for bitmap sub-sampling

---
+++ app/src/main/java/com/tune/tuneme/util/Density.java
@@ -0,0 +1,8 @@
+package com.tune.tuneme.util;
+
+public class Density {
+    public static final int LDPI_LOWER = 320;
+    public static final int LDPI_HIGHER = 400;
+    public static final int XXHDPI = 560;
+    public static final int MDPI = 480;
+}
--- app/src/main/java/com/tune/tuneme/util/ImageUtils.java
+++ app/src/main/java/com/tune/tuneme/util/ImageUtils.java
@@ -3,60 +3,41 @@ package com.tune.tuneme.util;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 
 /**
  * Set of utility methods used to produce better images in device memory
  */
 public class ImageUtils {
+    public static final String TAG = "ImageUtils";
 
     /**
-     * @param res       the app resource
-     * @param resId     the resource Id of the image
+     * @param res   the app resource
+     * @param resId the resource Id of the image
      * @return the sub-sampled image based on the size specified
      */
     public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId) {
-
         // First decode with inJustDecodeBounds=true to check dimensions
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeResource(res, resId, options);
+        int inSampleSize;
+        // sub-sample image according to device density
+        int density = res.getDisplayMetrics().densityDpi;
 
-        // Calculate inSampleSize
-        options.inSampleSize = 2;
+        Log.d(TAG, "Density: " + density + " dpi");
 
+        if (density <= Density.LDPI_LOWER) {
+            inSampleSize = 5;
+        } else if (density <= Density.LDPI_HIGHER && density > Density.LDPI_LOWER) {
+            inSampleSize = 4;
+        } else if (density <= Density.MDPI && density < Density.XXHDPI) {
+            inSampleSize = 3;
+        } else inSampleSize = 2;
+        options.inSampleSize = inSampleSize;
         // Decode bitmap with inSampleSize set
         options.inJustDecodeBounds = false;
         return BitmapFactory.decodeResource(res, resId, options);
     }
-
-    /**
-     * @param options   the options used in sub-sampling
-     * @param reqWidth  the requested width used in sub-sampling
-     * @param reqHeight the requested height used in sub-sampling
-     * @return the calculated in-sample size
-     */
-    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth,
-                                            int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }
-
 
 }
