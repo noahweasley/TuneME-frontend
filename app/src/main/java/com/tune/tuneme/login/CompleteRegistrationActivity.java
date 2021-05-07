0ff1abd52129e2f8e9cf692de4f2aaa3db4bb578
Noah <43308031+noahweasley@users.noreply.github.com>
2021-05-07 04:02:14 -0700

    Updated CompleteRegistrationActivity's view

---
+++ app/src/main/java/com/tune/tuneme/login/CompleteRegistrationActivity.java
@@ -0,0 +1,48 @@
+package com.tune.tuneme.login;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tune.tuneme.R;
+import com.tune.tuneme.databinding.CompleteRegistrationBinding;
+
+public class CompleteRegistrationActivity extends AppCompatActivity {
+    private CompleteRegistrationBinding binding;
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, CompleteRegistrationActivity.class);
+        context.startActivity(starter);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.fade_in_1, R.anim.fade_out_1);
+        binding = CompleteRegistrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // populate listCountries, with list of countries for auto-completion
+        String[] countryArray = getResources().getStringArray(R.array.countries_array);
+        ArrayAdapter<String> adapter
+                = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, countryArray);
+
+        binding.listCountries.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.fade_in_1, R.anim.fade_out_1);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
