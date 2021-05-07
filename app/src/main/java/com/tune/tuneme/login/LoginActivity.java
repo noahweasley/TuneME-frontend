d53ad87bb83aca7696c6f4ee959e2a21f3d99522
Noah <43308031+noahweasley@users.noreply.github.com>
2021-05-07 04:02:36 -0700

    Updated LoginActivity's view

--- app/src/main/java/com/tune/tuneme/login/LoginActivity.java
+++ app/src/main/java/com/tune/tuneme/login/LoginActivity.java
@@ -1,15 +1,23 @@
 package com.tune.tuneme.login;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.tune.tuneme.R;
 import com.tune.tuneme.databinding.LoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
     private LoginBinding binding;
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, LoginActivity.class);
+        context.startActivity(starter);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,11 +26,18 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // Make Licence agreement statements and login text clickable links
-        binding.lcAgree.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.txtLcAgree.setMovementMethod(LinkMovementMethod.getInstance());
         binding.signNow.setMovementMethod(LinkMovementMethod.getInstance());
 
+        binding.gSignParent.setOnClickListener(v -> requestServerFeedback(binding));
+        binding.signUp.setOnClickListener(v -> CompleteRegistrationActivity.start(this));
     }
 
+    @SuppressWarnings("unused")
+    private void requestServerFeedback(LoginBinding binding) {
+        // if can send data, then ...
+        GoogleLoginActivity.start(this);
+    }
 
     @Override
     protected void onDestroy() {
