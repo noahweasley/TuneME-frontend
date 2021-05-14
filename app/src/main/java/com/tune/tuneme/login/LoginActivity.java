87608bfed465fc50f231147654b631e924506fe2
Noah <iebenmelu@gmail.com>
2021-05-13 23:07:39 -0700

    sign up button is disabled until License agreement checkbox is checked

--- app/src/main/java/com/tune/tuneme/login/LoginActivity.java
+++ app/src/main/java/com/tune/tuneme/login/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.tune.tuneme.login;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
@@ -8,16 +7,10 @@ import android.text.method.LinkMovementMethod;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.tune.tuneme.R;
 import com.tune.tuneme.databinding.LoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
     private LoginBinding binding;
-
-    public static void start(Context context) {
-        Intent starter = new Intent(context, LoginActivity.class);
-        context.startActivity(starter);
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,14 +22,13 @@ public class LoginActivity extends AppCompatActivity {
         binding.txtLcAgree.setMovementMethod(LinkMovementMethod.getInstance());
         binding.signNow.setMovementMethod(LinkMovementMethod.getInstance());
 
-        binding.gSignParent.setOnClickListener(v -> requestServerFeedback(binding));
-        binding.signUp.setOnClickListener(v -> CompleteRegistrationActivity.start(this));
-    }
+        binding.gSignParent.setOnClickListener(v ->
+                startActivity(new Intent(this, GoogleLoginActivity.class)));
 
-    @SuppressWarnings("unused")
-    private void requestServerFeedback(LoginBinding binding) {
-        // if can send data, then ...
-        GoogleLoginActivity.start(this);
+        binding.signUp.setOnClickListener(v -> CompleteRegistrationActivity.start(this));
+        binding.cbxLcAgree.setOnCheckedChangeListener(
+                (buttonView, isChecked) -> binding.signUp.setEnabled(isChecked));
+
     }
 
     @Override
