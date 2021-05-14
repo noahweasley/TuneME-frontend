1c014275d034a74839102012d0fd3b90a5f97563
Noah <iebenmelu@gmail.com>
2021-05-13 22:59:00 -0700

    Still on Google Sign in. Error might be present

--- app/src/main/java/com/tune/tuneme/login/GoogleLoginActivity.java
+++ app/src/main/java/com/tune/tuneme/login/GoogleLoginActivity.java
@@ -1,21 +1,19 @@
 package com.tune.tuneme.login;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.tune.tuneme.databinding.GoogleLoginBinding;
 
 public class GoogleLoginActivity extends AppCompatActivity {
     private GoogleLoginBinding binding;
-
-    public static void start(Context context) {
-        Intent starter = new Intent(context, GoogleLoginActivity.class);
-        context.startActivity(starter);
-    }
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +22,21 @@ public class GoogleLoginActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.signUp.setOnClickListener(v -> requestServerFeedback(binding));
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+
     }
 
     @SuppressWarnings("unused")
