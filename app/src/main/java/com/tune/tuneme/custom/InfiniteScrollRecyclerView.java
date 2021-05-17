1d82eb2b7a37747dbc4b15a37559ee6523f621ec
Noah <iebenmelu@gmail.com>
2021-05-17 16:59:28 -0700

    want to implement Infinite scrolling list custom view

---
+++ app/src/main/java/com/tune/tuneme/custom/InfiniteScrollRecyclerView.java
@@ -0,0 +1,25 @@
+package com.tune.tuneme.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class InfiniteScrollRecyclerView extends RecyclerView {
+
+    public InfiniteScrollRecyclerView(@NonNull Context context) {
+        super(context);
+    }
+
+    public InfiniteScrollRecyclerView(@NonNull Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public InfiniteScrollRecyclerView(@NonNull Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
