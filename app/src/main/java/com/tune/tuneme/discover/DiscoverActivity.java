3c58b665ddba8c6b6f2016cb6c316b4c7ac29cbe
Noah <iebenmelu@gmail.com>
2021-05-19 23:44:18 -0700

    Started working on DiscoverActivity proper

--- app/src/main/java/com/tune/tuneme/discover/DiscoverActivity.java
+++ app/src/main/java/com/tune/tuneme/discover/DiscoverActivity.java
@@ -1,8 +1,11 @@
 package com.tune.tuneme.discover;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.tune.tuneme.R;
 import com.tune.tuneme.databinding.DiscoverBinding;
@@ -11,15 +14,25 @@ import com.tune.tuneme.databinding.DiscoverBinding;
 public class DiscoverActivity extends AppCompatActivity {
     private DiscoverBinding binding;
 
+    /**
+     * Simple helper method to start this activity
+     * @param context the origin context
+     */
+   public static void start(Context context) {
+       Intent starter = new Intent(context, DiscoverActivity.class);
+       context.startActivity(starter);
+   }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DiscoverBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        Toolbar toolbar = binding.toolbar;
+//        toolbar.setTitleTextAppearance(this, R.style.Toolbar_Title_Large);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.discover);
-
     }
 
     @Override
